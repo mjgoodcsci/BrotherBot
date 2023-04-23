5b53391e5698dd371f03e015261dd7b90774d865
Michael Good <mjgood@coastal.edu>
2023-04-23 17:01:23 -0500

    added bot that will cum online

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BrotherBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
