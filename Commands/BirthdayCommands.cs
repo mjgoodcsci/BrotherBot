c95b2769207e99e7f768ad7f63cc4e1d152d375a
Michael Good <mjgood@coastal.edu>
2023-04-23 23:11:26 -0500

    added setbirthday and it works, added framework for getbrithday (not working yet)

---
+++ Commands/BirthdayCommands.cs
@@ -0,0 +1,66 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.IO;
+
+namespace BrotherBot.Commands
+{
+    public class BirthdayCommands : BaseCommandModule
+    {
+        string filePath = @"..\..\..\Birthdays.txt";
+        // !setbirthday sets birthday
+        [Command("setbirthday")]
+        public async Task setBirthdayCommand(CommandContext ctx, string monthDay)
+        {
+            int dayOfTheYear = 0;
+            try
+            {
+                int month = Int32.Parse(monthDay.Split('/')[0]);
+                int day = Int32.Parse(monthDay.Split('/')[1]);
+                DateTime dateTime = new DateTime(2000, month, day);
+                dayOfTheYear = dateTime.DayOfYear;
+            }
+            catch 
+            {
+                await ctx.Channel.SendMessageAsync(monthDay + " is not in the correct formate, please do MM/DD");
+                return;
+            }
+
+            string nameDiscriminator = ctx.Member.Username + "#" + ctx.Member.Discriminator;
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            bool notThere = true;
+            foreach (string line in lines)
+            {
+                List<string> splitLine = line.Split(':').ToList();
+                if (splitLine[0] == nameDiscriminator)
+                {
+                    notThere = false;
+                    lines.Remove(line);
+                    lines.Add(nameDiscriminator + ":" + dayOfTheYear);
+                    break;
+                }
+            }
+            if (notThere) 
+            {
+                lines.Add(nameDiscriminator + ":" + dayOfTheYear);
+            }
+
+            File.WriteAllLines(filePath, lines.ToArray());
+            
+            await ctx.Channel.SendMessageAsync("Birthday set");
+        }
+
+
+        public List<string> GetBirthdays()
+        {
+            var day = DateTime.Now.DayOfYear;
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach(string line in lines)
+            {
+                
+            }
+
+            return new List<string>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Discord;
-using Discord.WebSocket;
-using Discord.Commands;
-using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
-
-namespace BrotherBot
+﻿namespace BrotherBot
 {
     class Program
     {
